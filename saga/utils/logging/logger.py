5d435b1db38933f670ff3e74a00aecd816fb0df7
Ole Weidner <ole.weidner@rutgers.edu>
2012-12-03 00:39:10 -0500

    implemented SAGA_VERBOSE

--- saga/utils/logging/logger.py
+++ saga/utils/logging/logger.py
@@ -10,26 +10,42 @@ __license__   = "MIT"
 
 import os
 from logging import StreamHandler, Filter, getLogger as logging_getLogger
-from logging import INFO, DEBUG, ERROR, WARNING
+from logging import INFO, DEBUG, ERROR, WARNING, CRITICAL
 from saga.utils.singleton import Singleton
 from saga.utils.logging.colorstreamhandler import *
 from saga.utils.logging.defaultformatter import DefaultFormatter
 
 class _LoggerConfig(object):
-    __metaclass__ = Singleton
+    #__metaclass__ = Singleton
 
     def __init__(self):
-        saga_verbose = os.environ.get('SAGA_VERBOSE')
-        if saga_verbose is not None:
-            pass
-        else:
-            self._loglevel = DEBUG
 
-        saga_log_level = os.environ.get('SAGA_LOG_LEVEL')
-        if saga_log_level is not None:
-            pass
-        else:
-            self._loglevel = DEBUG
+        self._loglevel = CRITICAL
+        SAGA_VERBOSE = os.environ.get('SAGA_VERBOSE')
+        if SAGA_VERBOSE is not None:
+            SAGA_VERBOSE = int(SAGA_VERBOSE)
+            # 4 = DEBUG + INFO + WARNING + ERROR
+            if SAGA_VERBOSE >= 4:
+                self._loglevel = DEBUG
+            # 3 = INFO + WARNING + ERROR
+            elif SAGA_VERBOSE == 3:
+                self._loglevel = INFO
+            # 2 = WARNING + ERROR 
+            elif SAGA_VERBOSE == 2:
+                self._loglevel = WARNING
+            # 1 = ERROR ONLY
+            elif SAGA_VERBOSE == 1:
+                print 'hey!'
+                self._loglevel = ERROR
+            # 0 = No Logging
+            else:
+                self._loglevel = CRITICAL
+
+        #saga_log_level = os.environ.get('SAGA_LOG_LEVEL')
+        #if saga_log_level is not None:
+        #    pass
+        #else:
+        #    self._loglevel = DEBUG
 
         saga_log_filter = os.environ.get('SAGA_LOG_FILTER')
         if saga_log_filter is not None:
@@ -55,7 +71,7 @@ class _LoggerConfig(object):
     def target(self):
         return self._target
 
-def getSAGALogger(module, obj=None):
+def getLogger(module, obj=None):
     ''' Get the SAGA logger.
     '''
     if obj is None:
@@ -70,15 +86,23 @@ def getSAGALogger(module, obj=None):
 
     handler.setFormatter(DefaultFormatter)
     _logger.addHandler(handler)
-
     _logger.setLevel(_LoggerConfig().loglevel)
     _logger.propagate = 0 # Don't bubble up to the root logger
 
     return _logger
 
 def _test_():
-    l1 = getSAGALogger('core')
-    l1.info('hi')
-    l2 = getSAGALogger('adaptor', 'pbs')
-    l2.error('problem')
+    l1 = getLogger('core')
+    l1.debug('debug')
+    l1.info('info')
+    l1.warning('warning')
+    l1.error('error')
+    l1.fatal('fatal')
+
+    l2 = getLogger('adaptor', 'pbs')    
+    l2.debug('debug')
+    l2.info('info')
+    l2.warning('warning')
+    l2.error('error')
+    l2.fatal('fatal')
 
