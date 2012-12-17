b720d164840d5d237645cb9f4322c6d7a61afc32
Andre Merzky <andre@merzky.net>
2012-12-17 11:31:05 +0100

    snapshot

--- saga/engine/engine.py
+++ saga/engine/engine.py
@@ -9,8 +9,11 @@ __license__   = "MIT"
 '''
 
 from saga.utils.singleton import Singleton
-from saga.engine.config import Configurable, Configuration, getConfig
-from saga.engine.logger import Logger, getLogger
+from saga.engine.config   import Configurable, Configuration, getConfig
+from saga.engine.logger   import Logger, getLogger
+
+import os
+import inspect
 
 ############# These are all supported options for saga.engine ####################
 ##
@@ -21,7 +24,16 @@ _all_engine_config_options = [
     'type'          : str, 
     'default'       : 'bar', 
     'valid_options' : None,
-    'documentation' : 'A sample configuration option. Does nothing.',
+    'documentation' : 'dummy config option for unit test.',
+    'env_variable'  : None
+    },
+    { 
+    'category'      : 'saga.engine',
+    'name'          : 'adaptor_paths', 
+    'type'          : list, 
+    'default'       : [], 
+    'valid_options' : None,
+    'documentation' : 'additional adaptor search paths.',
     'env_variable'  : None
     }
 ]
@@ -75,7 +87,7 @@ class Engine(Configurable):
 
           def __init__ (self, url, session=None) :
               
-              for adaptor_class in self._engine._adaptors {'job'}{'http'}
+              for adaptor_class in self._engine._adaptors{'job'}{'http'}
                   try :
                       self._adaptor = adaptor_class (url, session}
                   except saga.Exception e :
@@ -83,6 +95,13 @@ class Engine(Configurable):
                   else :
                       # successfully bound to adaptor
                       return
+
+        Adaptors to be loaded are searched for, by default in the module's
+        'adaptors/' subdirectory.  The config option 'adaptor_path' can 
+        specify additional (comma separated) paths to search.  The engine 
+        will attempt to load any python module named 'saga_adaptor_[name].py'
+        in any of the specified paths (default path first, then configured 
+        paths in the specified order).
         '
     '''
     __metaclass__ = Singleton
@@ -90,15 +109,31 @@ class Engine(Configurable):
     def __init__(self):
         # set the configuration options for this object
         Configurable.__init__(self, 'saga.engine', _all_engine_config_options)
+
         # initialize logging
         self._initialize_logging()
+
+        getLogger('engine').debug("Hello Engine")
+
+        # load adaptors
         self._load_adaptors()
+
 
     def _initialize_logging(self):
         Logger()
 
     def _load_adaptors(self):
-        pass
+        adaptor_paths = []  # paths to search for adaptors
+
+        # <saga module paths>/adaptors is the first path to search through
+        adaptor_paths.append (os.path.dirname (inspect.getfile (Engine)))
+
+        # then search through configured paths
+        adaptor_paths.append (self.get_config()['adaptor_paths'].get_value())
+
+        getLogger('engine').debug("%s"  %  str(adaptor_paths))
+
+
 
     def list_loaded_adaptors(self):
         pass
@@ -110,6 +145,10 @@ def test_singleton():
     # make sure singleton works
     assert(getEngine() == getEngine())
     assert(getEngine() == Engine())
+
+    e1 = Engine()
+    e2 = Engine()
+    assert(e1 == e2)
 
 def test_configurable():
     # make sure singleton works
