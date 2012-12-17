05af7eed1c91eb2ec91d5d822aa6b7c232614a91
Ole Weidner <ole.weidner@rutgers.edu>
2012-12-17 21:14:52 +0100

    started moving tests

--- saga/engine/engine.py
+++ saga/engine/engine.py
@@ -1,12 +1,3 @@
-# -*- coding: utf-8 -*-
-# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
-
-__author__    = "Ole Christian Weidner"
-__copyright__ = "Copyright 2012, The SAGA Project"
-__license__   = "MIT"
-
-''' Provides API handles for SAGA's runtime.
-'''
 
 from saga.utils.singleton import Singleton
 from saga.engine.config   import Configurable, Configuration, getConfig
@@ -167,22 +158,3 @@ class Engine(Configurable):
         pprint.pprint (dict(self._adaptors))
 
         pass
-
-
-############################# BEGIN UNIT TESTS ################################
-##
-def test_singleton():
-    # make sure singleton works
-    assert(getEngine() == getEngine())
-    assert(getEngine() == Engine())
-
-    e1 = Engine()
-    e2 = Engine()
-    assert(e1 == e2)
-
-def test_configurable():
-    # make sure singleton works
-    assert Engine().get_config()['foo'].get_value() == 'bar'  
-##
-############################## END UNIT TESTS #################################
-
