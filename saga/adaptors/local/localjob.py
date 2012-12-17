198d551fb7ec228327296bd70a83d8d43d1095ab
Andre Merzky <andre@merzky.net>
2012-12-17 20:35:10 +0100

    adaptor loading works - needs tests and code review

--- saga/adaptors/local/localjob.py
+++ saga/adaptors/local/localjob.py
@@ -1,28 +1,66 @@
 
+from saga.engine.config import Configurable
+
+######################################################################
+#
+# adaptor meta data
+#
+_adaptor_name     =    'saga.adaptor.local.localjob'
+_adaptor_registry = [{ 'name'    : _adaptor_name,
+                       'type'    : 'saga.job.Job',
+                       'class'   : 'local_job',
+                       'schemas' : ['fork', 'local']
+                     }, 
+                     { 'name'    : _adaptor_name,
+                       'type'    : 'saga.job.Service',
+                       'class'   : 'local_job_service',
+                       'schemas' : ['fork', 'local']
+                     }]
+
+
+######################################################################
+#
+# adaptor registration
+#
 def register () :
 
-    registry = [{ 'name'    : 'local job adaptor',
-                  'type'    : 'saga.job.Job',
-                  'class'   : 'local_job',
-                  'schemas' : ['fork', 'local']
-                }, 
-                { 'name'    : 'local job adaptor',
-                  'type'    : 'saga.job.Service',
-                  'class'   : 'local_job_service',
-                  'schemas' : ['fork', 'local']
-                }]
+    # perform some sanity checks, like check if dependencies are met
+    if "today" == "Monday" :
+        return None
 
-    return registry
+    return _adaptor_registry
 
 
-class local_job (object) :
+######################################################################
+#
+# job adaptor class
+#
+class local_job (Configurable) :
 
     def __init__ (self) :
         print "local job adaptor init";
 
 
-class local_job_service (object) :
+    def get_id (self) :
 
-    def __init__ (self) :
+        return id (self)
+
+
+######################################################################
+#
+# job service adaptor class
+#
+class local_job_service (Configurable) :
+
+    def __init__ (self, url) :
         print "local job service adaptor init";
+
+        # keep initialization state
+        self._url = url
+
+
+    def get_id (self) :
+
+        return self._url
+
 
--- saga/engine/engine.py
+++ saga/engine/engine.py
@@ -57,8 +57,31 @@ class Engine(Configurable):
         Python. It can be used to introspect the current state of
         a SAGA instance.
 
-        While loading adaptors, the Engine builds up a registry of 
-        adaptor classes, hierarchically sorted like this::
+        The Engine singleton will, on creation, load all available 
+        adaptors.  Adaptors modules MUST implement a register() 
+        function which returns a list of dicts like this::
+
+          [
+            { 
+              'name'    : _adaptor_name,
+              'type'    : 'saga.job.Job',
+              'class'   : 'local_job',
+              'schemas' : ['fork', 'local']
+            }, 
+            { 
+              'name'    : _adaptor_name,
+              'type'    : 'saga.job.Service',
+              'class'   : 'local_job_service',
+              'schemas' : ['fork', 'local']
+            } 
+          ]
+
+        where 'class' points to the actual adaptor classes, 
+        and 'schemas' lists the URL schemas for which those 
+        adaptor classes should be considered.
+
+        While loading adaptors, the Engine builds up a registry 
+        of adaptor classes, hierarchically sorted like this::
 
           _adaptors = 
           { 
@@ -103,7 +126,11 @@ class Engine(Configurable):
         paths in the specified order).
         '
     '''
+    
+    
     __metaclass__ = Singleton
+
+
 
     def __init__(self):
         
@@ -122,15 +149,16 @@ class Engine(Configurable):
 
     def _initialize_logging(self):
         Logger()
+        self._logger = getLogger('engine')
 
 
     def _load_adaptors(self):
 
-        global_conf = saga.engine.getConfig()
+        global_config = saga.engine.getConfig()
 
         for module_name in adaptor_registry :
 
-            print "engine: load adaptor  : " + module_name
+            self._logger.info("load adaptor " + module_name)
 
             try :
                 adaptor_module = __import__ (module_name, fromlist=['register'])
@@ -139,27 +167,63 @@ class Engine(Configurable):
                 # which returns a info dict for all implemented CPI classes
                 adaptor_infos = adaptor_module.register ()
 
-                pprint.pprint (adaptor_infos)
+                if adaptor_infos is None :
+                    # adaptor does not want to be registered -- probably did not
+                    # pass some sanity check
+                    self._logger.info("load adaptor " + module_name + " -- aborted")
+                    continue
 
+                # we got an adaptor info struct
                 for adaptor_info in adaptor_infos :
-                    adaptor_name    = adaptor_info['name']
-                    adaptor_type    = adaptor_info['type']
-                    adaptor_class   = adaptor_info['class']
-                    adaptor_schemas = adaptor_info['schemas']
 
-                    # check if adaptor is disabled via config
-                    disabled = self.get_config()['foo'].get_value() == 'bar'  
+                    # dig adaptor information from registry. Missing keys are
+                    # rightly expected to raise an exception
+                    adaptor_name     = adaptor_info['name']
+                    adaptor_type     = adaptor_info['type']
+                    adaptor_class    = adaptor_info['class']
+                    adaptor_schemas  = adaptor_info['schemas']
+                    adaptor_enabled  = True  # default
+                    adaptor_fullname = module_name + '.' + adaptor_class
 
+                    # try to find an 'enabled' option in the adaptor's config section
+                    try :
+                        adaptor_config  = global_config.get_category (module_name)
+                        adaptor_enabled = adaptor_config['enabled'].get_value ()
+                    except Exception as e :
+                        self._logger.info("load adaptor " + adaptor_fullname + " -- no config options: " + str(e))
+
+                    # only load adaptor if it is not disabled via config files
+                    if adaptor_enabled in ["False", False] :
+                        self._logger.info("load adaptor " + adaptor_fullname + " -- disabled")
+                        continue
+                    else :
+                        self._logger.info("load adaptor " + adaptor_fullname + " -- enabled")
+
+                    # register adaptor class for the listed URL schemas
                     for adaptor_schema in adaptor_schemas :
-
                         adp_class = getattr (adaptor_module, adaptor_class)
                         self._adaptors[adaptor_type][adaptor_schema] = adp_class
 
-                pprint.pprint (dict(self._adaptors))
 
             except Exception as e :
-                print "engine: loading failed: " + module_name
-                print str (e)
+                self._logger.error("load adaptor " + module_name + " -- failed: " + str(e))
+
+
+    def get_adaptor (self, type, schema) :
+        '''
+        Sift through the self._adaptors registry, and try to fund an adaptor
+        which can successfully be instantiated for 
+        '''
+
+        for adaptor_class in self._engine._adaptors['job']['http'] :
+            try :
+                self._adaptor = adaptor_class (url, session)
+            except saga.Exception as e :
+                pass
+                # adaptor could not handle the URL, handle e
+            else :
+                # successfully bound to adaptor
+                return
 
 
     def list_loaded_adaptors(self):
