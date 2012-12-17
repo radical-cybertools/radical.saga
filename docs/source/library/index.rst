310c74f7b8398fafd2afa199f4a0744f9a1eae07
Ole Weidner <ole.weidner@rutgers.edu>
2012-12-18 00:25:49 +0100

    started documenting

--- docs/source/library/index.rst
+++ docs/source/library/index.rst
@@ -1,3 +1,8 @@
 #################
 Library Reference
 #################
+
+.. automodule:: saga.exceptions
+   :members: SagaException, NotImplemented, IncorrectURL, BadParameter,
+             AlreadyExists, DoesNotExist, IncorrectState, PermissionDenied,
+             AuthorizationFailed, AuthenticationFailed, Timeout, NoSuccess
--- saga/exceptions.py
+++ saga/exceptions.py
@@ -12,6 +12,15 @@ import traceback
 from saga.utils.exception  import ExceptionBase
 
 class SagaException(ExceptionBase):
+    """ The SAGA base exception. All other SAGA exceptions inherit from 
+        this base class and can hence be caught via it. For example::
+
+          try:
+              raise NotImplemented('Sorry, not implemented')
+          except SagaException, ex:
+              print ex
+              print ex.traceback
+    """
 
     def __init__  (self, message, object=None) :
         self._message   = message
@@ -19,12 +28,18 @@ class SagaException(ExceptionBase):
         self._traceback = repr (traceback.format_stack ())
 
     def get_message (self) :
+        """ Return the exception message as a string.
+        """
         return self._message
 
     def get_object (self) :
+        """ Return the object that raised this exception.
+        """
         return self._object
 
     def get_traceback (self) :
+        """ Return the full traceback for this exception.
+        """
         return self._traceback
 
     def get_all_exceptions (self) :
@@ -41,18 +56,85 @@ class SagaException(ExceptionBase):
     object     = property (get_object)          # object type
     traceback  = property (get_traceback)       # string
     exceptions = property (get_all_exceptions)  # list [Exception]
-    messages   = property (get_all_message)     # list [string]
+    messages   = property (get_all_messages)     # list [string]
 
 
-class NotImplemented       (SagaException)   : pass
-class IncorrectURL         (SagaException)   : pass
-class BadParameter         (SagaException)   : pass
-class AlreadyExists        (SagaException)   : pass
-class DoesNotExist         (SagaException)   : pass
-class IncorrectState       (SagaException)   : pass
-class PermissionDenied     (SagaException)   : pass
-class AuthorizationFailed  (SagaException)   : pass
-class AuthenticationFailed (SagaException)   : pass
-class Timeout              (SagaException)   : pass
-class NoSuccess            (SagaException)   : pass
+class NotImplemented(SagaException):
+    """ The NotImplemented exception is raised when...
+
+        This exception inherits from :class:`SagaException`.
+    """
+    pass
+
+class IncorrectURL(SagaException): 
+    """ The IncorrectURL exception is raised when...
+
+        This exception inherits from :class:`SagaException`.
+    """
+    pass
+
+class BadParameter(SagaException): 
+    """ The BadParameter exception is raised when...
+
+        This exception inherits from :class:`SagaException`.
+    """
+    pass
+
+class AlreadyExists(SagaException): 
+    """ The AlreadyExists exception is raised when...
+
+        This exception inherits from :class:`SagaException`.
+
+    """
+    pass
+
+class DoesNotExist(SagaException):
+    """ The DoesNotExist exception is raised when...
+
+        This exception inherits from :class:`SagaException`.
+    """
+    pass
+
+class IncorrectState(SagaException): 
+    """ The IncorrectState exception is raised when...
+
+        This exception inherits from :class:`SagaException`.
+    """
+    pass
+
+class PermissionDenied(SagaException): 
+    """ The PermissionDenied exception is raised when...
+
+
+        This exception inherits from :class:`SagaException`.
+    """
+    pass
+
+class AuthorizationFailed(SagaException): 
+    """ The AuthorizationFailed exception is raised when...
+
+        This exception inherits from :class:`SagaException`.
+    """
+    pass
+
+class AuthenticationFailed(SagaException): 
+    """ The AuthenticationFailed exception is raised when...
+    
+        This exception inherits from :class:`SagaException`.
+    """
+    pass
+
+class Timeout(SagaException): 
+    """ The Timeout exception is raised when...
+    
+        This exception inherits from :class:`SagaException`.
+    """
+    pass
+
+class NoSuccess(SagaException): 
+    """ The NoSuccess exception is raised when...
+    
+        This exception inherits from :class:`SagaException`.
+    """
+    pass
 
