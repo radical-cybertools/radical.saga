c212c8e5eb07376fbf6ff2b5c7fd82a6d8fe6a9b
Ole Weidner <ole.weidner@rutgers.edu>
2012-12-18 12:28:42 +0100

    fixed env-var tests

--- tests/unittests/engine/test_engine.py
+++ tests/unittests/engine/test_engine.py
@@ -23,7 +23,16 @@ def test_configurable():
     # make sure singleton works
     assert Engine().get_config()['foo'].get_value() == 'bar'  
 
-
 def test_emtpy_regsitry():
     Engine()._load_adaptors([])
+    assert Engine().loaded_adaptors() == {}
+
+def test_load_nonexisting_module():
+    try:
+        Engine()._load_adaptors(12)
+        assert False
+    except TypeError:
+        assert True
+
+    Engine()._load_adaptors(["nonexisting"])
     assert Engine().loaded_adaptors() == {}
