cce74050f4917df6efcb8e4a97278ce05b6707fc
Andre Merzky <andre@merzky.net>
2012-12-31 16:34:25 +0100

    add async cpi

---
+++ saga/cpi/async.py
@@ -0,0 +1,24 @@
+
+
+from saga.cpi.base import Base
+
+import inspect
+import saga.exceptions
+
+class Async (object) :
+    
+    def __init__ (self, api) :
+        raise saga.exceptions.NotImplemented ("%s.%s is not implemented" % (__name__,inspect.stack()[0][3]))
+
+    def task_run (self, task) :
+        raise saga.exceptions.NotImplemented ("%s.%s is not implemented" % (__name__,inspect.stack()[0][3]))
+
+    def task_wait (self, task, timeout) :
+        raise saga.exceptions.NotImplemented ("%s.%s is not implemented" % (__name__,inspect.stack()[0][3]))
+
+    def task_cancel (self, task) :
+        raise saga.exceptions.NotImplemented ("%s.%s is not implemented" % (__name__,inspect.stack()[0][3]))
+
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+
