1de00602426f19cdfa0c94e8aeacfb06424902b8
Andre Merzky <andre@merzky.net>
2013-01-26 00:35:05 +0100

    make shell job wrapper more resilient against failure, reconnect, shell quirks, state changes, etc.

--- saga/adaptors/ssh/wrapper.sh
+++ saga/adaptors/ssh/wrapper.sh
@@ -1,30 +1,47 @@
 #!/bin/sh
 
+# this script uses only POSIX shell functionality, and does not rely on bash or
+# other shell extensions.  It expects /bin/sh to be a POSIX compliant shell
+# thought.
+
+
 # --------------------------------------------------------------------
+#
 # ERROR and RETVAL are used for return state from function calls
+#
 ERROR=""
 RETVAL=""
 
-# this is where this 'daemon' keeps state
+# this is where this 'daemon' keeps state for all started jobs
 BASE=$HOME/.saga/adaptors/ssh_job/
 
+
 # --------------------------------------------------------------------
+#
+# utility call which extracts the first argument and returns it.
+#
 get_cmd () {
   if test -z $1 ; then ERROR="no command given"; return; 
   else                 RETVAL=$1;                fi
 }
+
+
 # --------------------------------------------------------------------
+#
+# utility call which strips the first of a set of arguments, and returns the
+# remaining ones in a space separated string
+#
 get_args () {
-  if test -z $1 ; then ERROR="no command given"; return; fi 
-  shift # discard command
-  RETVAL=""
-  while test $# -gt 0
-  do
-    RETVAL="$RETVAL$1 "
-    shift
-  done
+  if test -z $1 ; then ERROR="no command given"; return; 
+  else                 shift; RETVAL=$@;         fi 
 }
+
+
 # --------------------------------------------------------------------
+#
+# utility call which ensures that a given job id points to a viable working
+# directory
+#
 verify_pid () {
   if test -z $1 ;            then ERROR="no pid given";              return 1; fi 
   DIR="$BASE/$1"
@@ -32,41 +49,124 @@ verify_pid () {
   if ! test -r "$DIR/pid";   then ERROR="pid $1 in incorrect state"; return 1; fi 
   if ! test -r "$DIR/state"; then ERROR="pid $1 in incorrect state"; return 1; fi
 }
+
+
 # --------------------------------------------------------------------
+#
+# run a job in the background.  Note that the returned job ID is actually the
+# pid of the shell process which wraps the actual job, monitors its state, and
+# serves its I/O channels.  The actual job id is stored in the 'pid' file in the
+# jobs working directory.
+#
+# Note that the actual job is not run directly, but via nohup.  Otherwise all
+# jobs would be canceled as soon as this master script finishes...
+#
+# Note that the working directory is created on the fly.  As the name of the dir
+# is the pid, it must be unique -- we thus purge whatever trace we find of an
+# earlier directory of the same name.
+#
+# Known limitations:
+#
+# The script has a small race condition, between starting the job (the 'nohup'
+# line), and the next line where the jobs now know pid is stored away.  I don't
+# see an option to make those two ops atomic, or resilient against script
+# failure - so, in worst case, you might get a running job for which the job id
+# is not stored (i.e. not known).  
+#
+# Also, the line after is when the job state is set to 'Running' -- we can't
+# really do that before, but on failure, in the worst case, we might have a job
+# with known job ID which is not marked as running.  
+#
+# Finally, the wait call must be in this shell instance -- if this instance dies
+# we will not be able to recover the exit state of the job.  We will, however,
+# be able to recover its SAGA state...
+#
+# FIXME?
+
 cmd_run () {
-  echo " run 1" >> /tmp/t
-  cmd_run_process $args &
+  cmd_run_process $@ &
   RETVAL=$!
-  echo " run 2" >> /tmp/t
 }
 
 cmd_run_process () {
   PID=`sh -c 'echo $PPID'`
   DIR="$BASE/$PID"
 
-  mkdir -p "$DIR"   || exit 1
-  echo "$args"       > "$DIR/cmd"
-  echo "RUNNING"     > "$DIR/state"
+  rm    -rf "$DIR"
+  mkdir -p  "$DIR"  || exit 1
+  echo "NEW"         > "$DIR/state"
+  echo "$@"          > "$DIR/cmd"
   touch                "$DIR/in"
-  $args \
-    <  "$DIR/in"   \
-    >  "$DIR/out"  \
-    2> "$DIR/err"  \
-    && echo "DONE"   > "$DIR/state" \
-    || echo "FAILED" > "$DIR/state" &
-  rpid=$!
-  echo $rpid         > "$DIR/pid"
-  wait $rpid
-  echo $?            > "$DIR/exit"
+
+  # create a script which represents the job.  The 'exec' call will replace the
+  # script's shell instance with the job executable, leaving the I/O
+  # redirections intact.
+  cat                > "$DIR/job.sh" <<EOT
+  exec $@            \
+    <  "$DIR/in"     \
+    >  "$DIR/out"    \
+    2> "$DIR/err"
+EOT
+  
+  # the job script above is started by this startup script, which makes sure
+  # that the job state is properly watched and captured.
+  cat                > "$DIR/monitor.sh" <<EOT
+    DIR=$DIR
+    nohup /bin/sh      "\$DIR/job.sh" 1>/dev/null 2>/dev/null 3</dev/null &
+    rpid=\$!
+    echo \$rpid      > "\$DIR/pid"
+    echo "RUNNING"   > "\$DIR/state"
+
+    while true
+    do
+      wait \$rpid
+      retv=\$?
+
+      # if wait failed for other reason than job finishing, i.e. due to
+      # suspend/resume, then we need to wait again, otherwise we are done
+      # waiting...
+      if test -e "\$DIR/suspended"; then
+        # need to wait again
+        sleep 1
+      else
+        # evaluate exit val
+        echo \$retv > "\$DIR/exit"
+        test \$retv = 0           && echo DONE      > "\$DIR/state"
+        test \$retv = 0           || echo FAILED    > "\$DIR/state"
+
+        # capture canceled state
+        test -e "\$DIR/canceled"  && echo CANCELED  > "\$DIR/state"
+        test -e "\$DIR/canceled"  && rm -f            "\$DIR/canceled"
+
+        # done waiting
+        break
+      fi
+    done
+
+EOT
+
+  # the monitor script is ran asynchronously and with nohup, so that its
+  # lifetime will not be bound to the manager script lifetime.
+  nohup /bin/sh "$DIR/monitor.sh" 1>/dev/null 2>/dev/null 3</dev/null &
 }
+
+
 # --------------------------------------------------------------------
+#
+# inspect job state
+#
 cmd_state () {
   verify_pid $1 || return
 
   DIR="$BASE/$1"
   RETVAL=`cat "$DIR/state"`
 }
+
+
 # --------------------------------------------------------------------
+#
+# suspend a running job
+#
 cmd_suspend () {
   verify_pid $1 || return
 
@@ -79,19 +179,25 @@ cmd_suspend () {
     return
   fi
 
+  touch "$DIR/suspended"
   RETVAL=`kill -STOP $rpid 2>&1`
   ECODE=$?
 
-  if ! test "$ECODE" = "0" ; then
-    ERROR="suspend failed ($ECODE): $RETVAL"
-  else
+  if test "$ECODE" = "0" ; then
+    mv    "$DIR/state" "$DIR/state.susp"
+    echo SUSPENDED >   "$DIR/state"
     RETVAL="$1 suspended"
+  else
+    ERROR="suspend failed ($ECODE): $RETVAL"
   fi
 
-  mv   "$DIR/state" "$DIR/state.susp"
-  echo "SUSPENDED" > "$DIR/state"
 }
+
+
 # --------------------------------------------------------------------
+#
+# resume a suspended job
+#
 cmd_resume () {
   verify_pid $1 || return
 
@@ -107,16 +213,21 @@ cmd_resume () {
   RETVAL=`kill -CONT $rpid 2>&1`
   ECODE=$?
 
-  if ! test "$ECODE" = "0" ; then
-    ERROR="resume failed ($ECODE): $RETVAL"
-    return
-  else
+  if test "$ECODE" = "0" ; then
+    mv    "$DIR/state.susp" "$DIR/state"
+    rm -f "$DIR/suspended"
     RETVAL="$1 resumed"
+  else
+    ERROR="resume failed ($ECODE): $RETVAL"
   fi
 
-  mv "$DIR/state.susp" "$DIR/state"
 }
+
+
 # --------------------------------------------------------------------
+#
+# kill a job, and set state to canceled
+#
 cmd_cancel () {
   verify_pid $1 || return
 
@@ -125,29 +236,29 @@ cmd_cancel () {
   state=`cat $DIR/state`
   rpid=`cat $DIR/pid`
 
-  if ! test "$state" = "SUSPENDED" -o "$state" = "RUNNING"; then
-    ERROR="job $1 in incorrect state ($state != SUSPENDED|RUNNING)"
+  if test "$state" != "SUSPENDED" -a "$state" != "RUNNING"; then
+    ERROR="job $1 in incorrect state ('$state' != 'SUSPENDED|RUNNING')"
     return
   fi
 
+  touch "$DIR/canceled"
   RETVAL=`kill -KILL $rpid 2>&1`
   ECODE=$?
 
-  if ! test "$ECODE" = "0" ; then
-    state=`cat $DIR/state`
-    if ! test "$state" = "DONE" -o "$state" = "FAILED"; then
-      ERROR="job $1 in incorrect state ($state != SUSPENDED|RUNNING)"
-    else
-      ERROR="cancel failed ($ECODE): $RETVAL"
-    fi
-    return
-  else
+  if test "$ECODE" = "0" ; then
     RETVAL="$1 canceled"
+  else
+    # kill failed!
+    rm -f "$DIR/canceled"
+    ERROR="cancel failed ($ECODE): $RETVAL"
   fi
-
-  echo "CANCELED" > "$DIR/state"
 }
+
+
 # --------------------------------------------------------------------
+#
+# feed given string to job's stdin stream
+# 
 cmd_stdin () {
   verify_pid $1 || return
 
@@ -156,21 +267,36 @@ cmd_stdin () {
   echo "$*" >> "$DIR/in"
   RETVAL="stdin refreshed"
 }
+
+
 # --------------------------------------------------------------------
+#
+# print uuencoded string of job's stdout
+#
 cmd_stdout () {
   verify_pid $1 || return
 
   DIR="$BASE/$1"
   RETVAL=`uuencode "$DIR/out" "/dev/stdout"`
 }
+
+
 # --------------------------------------------------------------------
+#
+# print uuencoded string of job's stderr
+#
 cmd_stderr () {
   verify_pid $1 || return
 
   DIR="$BASE/$1"
   RETVAL=`uuencode "$DIR/err" "/dev/stdout"`
 }
+
+
 # --------------------------------------------------------------------
+#
+# list all job IDs
+#
 cmd_list () {
   for d in "$BASE"/*; do
     RETVAL="$RETVAL`basename $d` "
@@ -178,7 +304,12 @@ cmd_list () {
 
   if test "$RETVAL" = "* "; then RETVAL=""; fi
 }
+
+
 # --------------------------------------------------------------------
+#
+# purge working directories of given jobs (all non-final jobs as default)
+#
 cmd_purge () {
 
   if test -z "$1" ; then
@@ -197,16 +328,18 @@ cmd_purge () {
   rm -rf "$DIR"
   RETVAL="purged $1"
 }
+
+
 # --------------------------------------------------------------------
+#
+# main even loop -- wait for incoming command lines, and react on them
+#
 listen() {
   
-  t=/tmp/t
-
   echo "CMD"
 
   while read LINE; do
 
-    echo "ECHO $LINE" >> /tmp/t
     ERROR="OK"
     RETVAL=""
 
@@ -215,7 +348,6 @@ listen() {
 
     if ! test "$ERROR" = "OK"; then
       echo "ERROR"     ; echo $ERROR
-      echo "ERROR" >>$t; echo $ERROR >>$t
       continue
     fi
 
@@ -237,17 +369,24 @@ listen() {
 
     if ! test "$ERROR" = "OK"; then
       echo "ERROR"     ; echo $ERROR
-      echo "ERROR" >>$t; echo $ERROR >>$t
     else
       echo "OK"        ; echo "$RETVAL"
-      echo "OK"    >>$t; echo "$RETVAL" >>$t
     fi
-    echo "CMD" >>$t
+
     echo "CMD"
+
   done
 }
+
+
 # --------------------------------------------------------------------
+#
+# run the main loop -- that will live forever, until a 'QUIT' command is
+# encountered.
+#
 listen
+#
 # --------------------------------------------------------------------
 
+# vim: tabstop=2 expandtab shiftwidth=2 softtabstop=2
 
