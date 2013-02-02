4e8d37cf6dca91edc20adee357c9f6c27adc50ce
Ole Weidner <ole.weidner@rutgers.edu>
2013-02-02 15:20:49 +0000

    added missing file header / license info

--- saga/attributes.py
+++ saga/attributes.py
@@ -1,9 +1,8 @@
-
 __author__    = "Andre Merzky"
-__copyright__ = "Copyright 2012, The SAGA Project"
+__copyright__ = "Copyright 2012-2013, The SAGA Project"
 __license__   = "MIT"
 
-""" SAGA attribute interface """
+""" Attribute interface """
 
 from saga.exceptions import *
 
--- saga/base.py
+++ saga/base.py
@@ -1,3 +1,6 @@
+__author__    = "Andre Merzky"
+__copyright__ = "Copyright 2012-2013, The SAGA Project"
+__license__   = "MIT"
 
 import string
 
