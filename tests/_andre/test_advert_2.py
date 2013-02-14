141620049f7b35aad165b2259e7072020cd9a5c2
Andre Merzky <andre@merzky.net>
2013-02-14 20:50:16 +0100

    cleanup advert tests

--- tests/_andre/test_advert_2.py
+++ tests/_andre/test_advert_2.py
@@ -13,15 +13,12 @@ for arg in sys.argv :
 # d_1 = saga.advert.Directory ('redis://localhost/tmp/test1/test1/')
 d_1 = saga.advert.Directory ('redis://repex1.tacc.utexas.edu:10001/tmp/test1/test1/')
 
-print "1"
 # should we notify the master that we are about to start?
 if start : d_1.set_attribute ('foo', 'start')
-print "2"
 
 # send 1000 attribute updates
-for i in xrange (10) :
+for i in xrange (100) :
     d_1.set_attribute ('foo', str(i))
-    print "."
 
 # should we notify the master that we are done?
 if stop : d_1.set_attribute ('foo', 'stop')
