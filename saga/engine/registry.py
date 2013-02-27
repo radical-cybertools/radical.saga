2c39387418faa77ef538400d7829df7b37d5ab89
Ole Weidner <ole.weidner@rutgers.edu>
2013-02-27 17:17:02 -0500

    added variable to control PTYWrapper behavior

--- saga/engine/registry.py
+++ saga/engine/registry.py
@@ -23,12 +23,5 @@ adaptor_registry = [
                     "saga.adaptors.context.userpass",
                     "saga.adaptors.local.localfile",
                     "saga.adaptors.shell.shell_job",
-<<<<<<< HEAD
                     "saga.adaptors.condor.condorjob",
-=======
-                    "saga.adaptors.pbs.pbsjob",
-
-                   ]
->>>>>>> devel
-
                    ]
