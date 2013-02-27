39789ab49d1d051ec88caa15dd8243c58d97b1ba
Ole Weidner <ole.weidner@rutgers.edu>
2013-02-27 17:14:06 -0500

    added variable to control PTYWrapper logging

--- saga/adaptors/sge/sgejob.py
+++ saga/adaptors/sge/sgejob.py
@@ -289,6 +289,9 @@ class SGEJobService (saga.adaptors.cpi.job.Service):
     def init_instance(self, adaptor_state, rm_url, session):
         """ service instance constructor
         """
+        # Turn this off by default.
+        self._disable_ptywrapper_logging = True
+        
         self.rm      = rm_url
         self.session = session
         self.ppn     = 0
@@ -323,17 +326,21 @@ class SGEJobService (saga.adaptors.cpi.job.Service):
                           'qdel':  None,
                           'qconf': None}
 
-        # create a null logger to silence the PTY wrapper!
-        import logging
+        if self._disable_ptywrapper_logging:
+            # create a null logger to silence the PTY wrapper!
+            import logging
 
-        class NullHandler(logging.Handler):
-            def emit(self, record):
-                pass
-        nh = NullHandler()
-        null_logger = logging.getLogger("PTYShell").addHandler(nh)
+            class NullHandler(logging.Handler):
+                def emit(self, record):
+                    pass
+            nh = NullHandler()
+            null_logger = logging.getLogger("PTYShell").addHandler(nh)
 
-        self.shell = saga.utils.pty_shell.PTYShell(pty_url,
-            self.session.contexts, null_logger)
+            self.shell = saga.utils.pty_shell.PTYShell(pty_url,
+                self.session.contexts, null_logger)
+        else:
+            self.shell = saga.utils.pty_shell.PTYShell(pty_url,
+                self.session.contexts)
 
         self.shell.set_initialize_hook(self.initialize)
         self.shell.set_finalize_hook(self.finalize)
