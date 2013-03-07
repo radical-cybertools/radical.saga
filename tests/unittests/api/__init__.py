b682502598f2a28c225a6168e50e5f1b23ff795a
Ole Weidner <ole.weidner@rutgers.edu>
2013-03-07 18:54:12 -0500

    started working on fs tests

--- tests/unittests/api/__init__.py
+++ tests/unittests/api/__init__.py
@@ -1,8 +1,4 @@
 
-__author__    = "Ole Christian Weidner"
-__copyright__ = "Copyright 2012, The SAGA Project"
+__author__    = "Ole Weidner"
+__copyright__ = "Copyright 2012-2013, The SAGA Project"
 __license__   = "MIT"
-
-
-# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
-
