65b56403749b241e7a499902ecabe000ca6db53b
Ole Weidner <ole.weidner@rutgers.edu>
2013-03-08 11:34:31 -0500

    more work on fs tests

--- tests/unittests/api/filesystem/test_file.py
+++ tests/unittests/api/filesystem/test_file.py
@@ -9,6 +9,25 @@ import saga.utils.test_config as sutc
 
 from copy import deepcopy
 
+
+# ------------------------------------------------------------------------------
+#
+def test_nonexisting_host_file_open():
+    """ Testing if opening a file on a non-existing host causes an exception.
+    """
+    try:
+        pass
+        tc = sutc.TestConfig()
+        invalid_url = deepcopy(saga.Url(tc.filesystem_url))
+        invalid_url.host += ".does.not.exist"
+        f = saga.filesystem.File(invalid_url)
+        assert False, "Expected BadParameter exception but got none."
+    except saga.BadParameter:
+        assert True
+    except saga.SagaException as ex:
+        assert False, "Expected BadParameter exception, but got %s" % ex
+
+
 # ------------------------------------------------------------------------------
 #
 def test_nonexisting_file_open():
@@ -17,14 +36,31 @@ def test_nonexisting_file_open():
     try:
         pass
         tc = sutc.TestConfig()
-        invalid_url = deepcopy(saga.Url(tc.existing_file_url))
-        invalid_url.host += ".does.not.exist"
-        f = saga.filesystem.File(invalid_url)
-        assert False, "Expected BadParameter exception but got none."
-    except saga.BadParameter:
+        nonex_file = deepcopy(saga.Url(tc.filesystem_url))
+        nonex_file.path += "/file.does.not.exist"
+        f = saga.filesystem.File(nonex_file)
+        assert False, "Expected DoesNotExist exception but got none."
+    except saga.DoesNotExist:
         assert True
     except saga.SagaException as ex:
-        assert False, "Expected BadParameter exception, but got %s" % ex
+        assert False, "Expected DoesNotExist exception, but got %s" % ex
+
+
+# ------------------------------------------------------------------------------
+#
+def test_nonexisting_file_create_open():
+    """ Testing if opening a non-existing file with the 'create' flag set works.
+    """
+    try:
+        pass
+        tc = sutc.TestConfig()
+        nonex_file = deepcopy(saga.Url(tc.filesystem_url))
+        nonex_file.path += "/file.does.not.exist"
+        f = saga.filesystem.File(nonex_file, saga.filesystem.CREATE)
+        assert f.size == 0
+    except saga.SagaException as ex:
+        assert False, "Unexpected exception: %s" % ex
+
 
 # ------------------------------------------------------------------------------
 #
@@ -39,7 +75,5 @@ def test_existing_file_open():
         #js = saga.job.Service(invalid_url, tc.session)
         #assert False, "Expected XYZ exception but got none."
 
-    except saga.BadParameter:
-        assert True
     except saga.SagaException as ex:
-        assert False, "Expected BadParameter exception, but got %s" % ex
+        assert False, "Unexpected exception: %s" % ex
