9ab7451a1eef745cc78526196764ad29e11661ae
Andre Merzky <andre@merzky.net>
2013-03-16 05:32:45 +0100

    remove some duplicated copyright statements

--- saga/adaptors/sge/__init__.py
+++ saga/adaptors/sge/__init__.py
@@ -4,9 +4,3 @@ __copyright__ = "Copyright 2013, The SAGA Project"
 __license__   = "MIT"
 
 
-#!/usr/bin/env python
-# encoding: utf-8
-
-__author__    = "Ole Weidner"
-__copyright__ = "Copyright 2013, The SAGA Project"
-__license__   = "MIT"
