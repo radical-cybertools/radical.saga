78310064348fa9c91be4c630e2ab843ffbf719d8
Andre Merzky <andre@merzky.net>
2013-04-14 23:53:39 +0200

    GC now works, connection restart does not.
    So, I had to revise my notion of python's variable scope, and with that newly
    gained insight (a nice word for brain damage in this case) supports the result
    that shell garbage collection now works as expected.

--- tests/_andre/test_gc.py
+++ tests/_andre/test_gc.py
@@ -8,41 +8,26 @@ import gc
 import sys
 import saga
 import time
-import objgraph as og
+import objgraph as checks
 from   pprint import pprint as pp
 
 # gc.set_debug (gc.DEBUG_LEAK | gc.DEBUG_STATS)
 
-
-## if True :
-##     js0 = saga.job.Service ('ssh://localhost/bin/sh')
-##     t0  = saga.job.Service.create ('ssh://localhost/bin/sh')
-##     t0.wait ()
-##     js0 = t0.get_result ()
-##     print js0.url 
-## 
-## gc.collect ()
-## pp (js0.__dict__)
-## print "---------------"
-## # pp (js0.__dict__['_init_task'].__dict__)
-## 
-## sys.exit (0)
-
-
-if True :
+def func () :
     js1 = saga.job.Service ('ssh://localhost/bin/sh')
-    js2 = js1
-    # refs = gc.get_referrers (js1)
 
-    # for ref in refs :
-    #     print "------------------------------"
-    #     print type (ref)
-    #     pp (ref)
 
- #  js1._adaptor.__del__()
-    
-time.sleep (5)
+##  js0 = saga.job.Service ('ssh://localhost/bin/sh')
+##  t0  = saga.job.Service.create ('ssh://localhost/bin/sh')
+##  t0.wait ()
+##  js0 = t0.get_result ()
+##  print js0.url 
+
+print "--------------------------"
+func ()
+print "--------------------------"
 gc.collect ()
+
 # print "=============================="
 # for g in gc.garbage :
 #     print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
@@ -51,20 +36,9 @@ gc.collect ()
 # pp (gc.garbage)
 # print "=============================="
 
-og.show_most_common_types ()
-print type (js1._adaptor)
-print type (js2._adaptor)
-# og.show_backrefs (js1._adaptor)
-# og.show_backrefs (js2._adaptor)
-print og.count('saga.job.Service')
-
-## pp (js1.__dict__)
-## # print "--------------------------------------------------------------------------------"
-## # pp (type(js1.__dict__['_adaptor']))
-## # pp (js1.__dict__['_adaptor'].__dict__)
-## print "--------------------------------------------------------------------------------"
-## # pp (type(js1.__dict__['_adaptor'].__dict__['_adaptor']))
-## # pp (js1.__dict__['_adaptor'].__dict__['_adaptor'].__dict__)
+# checks.show_backrefs (js1._adaptor)
+print checks.count('ShellJobService')
+checks.show_backrefs(checks.by_type('ShellJobService')[-1])
 
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
