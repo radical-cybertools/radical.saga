b7573d44bb5c6e048ddc7468dbee12759f60cef7
Ole Weidner <ole.weidner@rutgers.edu>
2013-04-16 20:30:40 -0400

    pending changes

--- examples/jobs/localjob.py
+++ examples/jobs/localjob.py
@@ -18,7 +18,7 @@ def main():
         # Create a job service object that represent the local machine.
         # The keyword 'fork' in the url scheme triggers the 'shell' adaptor.
         # The adaptor also support ssh:// and gsissh://
-        js = saga.job.Service("fork://localhost")
+        js = saga.job.Service("fork://localhost/bin/csh")
 
         # Next, we describe the job we want to run. A complete set of job
         # description attributes can be found in the API documentation.
@@ -30,7 +30,7 @@ def main():
         jd.environment       = {'FILENAME': 'testfile'}
         jd.wall_time_limit   = 1 # minutes
 
-        jd.executable        = '/bin/touch'
+        jd.executable        = '/usr/bin/touch'
         jd.arguments         = ['$FILENAME']
 
         jd.working_directory = "$HOME/A/B/C"
