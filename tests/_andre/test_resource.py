de84d5405ef4f70f1a4e2ef40399e50eddb75e3e
Andre Merzky <andre@merzky.net>
2013-05-16 14:03:22 +0200

    snapshot -- close to functional

--- tests/_andre/test_resource.py
+++ tests/_andre/test_resource.py
@@ -1,13 +1,14 @@
 
 import saga
 
-cd = saga.resource.ComputeDescription ()
-rm = saga.resource.Manager ("ec2://localhost")
-cr = rm.acquire (cd)
-print cr.access
-js = saga.job.Service (cr.access)
-jj  = js.run_job ("/bin/date")
+rm = saga.resource.Manager ("ec2://aws.amaon.com/")
 
-jj.wait ()
-cr.release ()
+cd = saga.resource.ComputeDescription ()
+cr = rm.acquire (cd)
+# print cr.access
+# js = saga.job.Service (cr.access)
+# jj  = js.run_job ("/bin/date")
+# 
+# jj.wait ()
+# cr.release ()
 
