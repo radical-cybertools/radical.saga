b96f66327b354465aab8915b49bc709a94f962db
Andre Merzky <andre@merzky.net>
2013-06-18 16:05:34 +0200

    improve up ec2 example

--- examples/resource/ec2.py
+++ examples/resource/ec2.py
@@ -84,6 +84,7 @@ This program has two modes of operation:
 # defines
 #
 START = 'start'
+RUN   = 'run'
 STOP  = 'stop'
 
 
@@ -99,8 +100,9 @@ def usage (msg = None) :
     print """
     Usage:
 
-        %s -s              :  start a new VM
-        %s -d <id> [...]   :  destroy specified VMs
+        %s -s             :  start a new VM
+        %s -r <id> [...]  :  run something on a VMs
+        %s -d <id> [...]  :  destroy specified VMs
 
     """
 
@@ -130,10 +132,13 @@ def main () :
         create VM instance
         run job on VM
         exit
-    if  -d in argv
+    if  -r in argv
         for each vm_id in argv
             connect to vm with id vm_id
             run job
+        exit
+    if  -d in argv
+        for each vm_id in argv
             destroy vm instance
         exit
     """
@@ -149,6 +154,14 @@ def main () :
             usage ("no additional args allowed on '-s'")
 
 
+    if  '-r' in args :
+        mode = RUN
+        args.remove ('-r')
+        if  len (args) == 0 :
+            usage ("additional args required on '-r'")
+        # we may have VM IDs to connect to
+        vm_ids = args
+
     if  '-d' in args :
         mode = STOP
         args.remove ('-d')
@@ -162,7 +175,6 @@ def main () :
         usage ()
 
 
-
     # in order to connect to EC2, we need an EC2 ID and KEY
     c1 = saga.Context ('ec2')
     c1.user_id  = os.environ['EC2_ID']
@@ -171,20 +183,12 @@ def main () :
     # in order to access a created VM, we additionally need to point to the ssh
     # key which is used for EC2 VM contextualization, i.e. as EC2 'keypair'.
     # If the keypair is not yet registered on EC2, it will be registered by SAGA
-    # -- but then a user_key *must* be specified.
+    # -- but then a user_key *must* be specified (only the public key is ever
+    # transfererd to EC2).
     c2 = saga.Context ('ec2_keypair')
-    c2.token    = 'futuregrid'  # keypair name
-    c2.user_key = '/Users/oweidner/.ssh/rsa_ec2.pub'
-    c2.user_id  = 'root'        # the user id on the target VM
-
-    # NOTE: the above ec2_keypair context should spawn an ssh context for
-    # accessing the respective VMs.  Alas this is not possible inm the general
-    # case with the present libcloud (for details, see comments in ec2 adaptor).
-    # We thus add that ssh context manually
-    c3 = saga.Context ('ssh')
-    c3.user_id   = 'root'
-    c3.user_cert = '/Users/oweidner/.ssh/rsa_ec2.pub'
-
+    c2.token     = 'futuregrid_1'  # keypair name
+    c2.user_cert = '/home/merzky/.ssh/id_rsa_futuregrid.pub'
+    c2.user_id   = 'root'         # the user id on the target VM
 
     # we create a session for all SAGA interactions, and attach the respective
     # security contexts.  Those are now avail for all SAGA objects created in
@@ -192,10 +196,13 @@ def main () :
     s = saga.Session (False)  # FALSE: don't use any other (default) contexts
     s.contexts.append (c1)
     s.contexts.append (c2)
-    s.contexts.append (c3)
 
-  # print "\nContexts: "
-  # pprint.pprint (s.contexts)
+    import pprint
+    print "\nContexts: "
+    print s
+    print '----------------'
+    sys.exit (0)
+
 
     # in this session, connect to the EC2 resource manager
     rm  = saga.resource.Manager ("ec2://aws.amazon.com/", session=s)
@@ -233,6 +240,68 @@ def main () :
         crs.append (cr)
 
 
+    elif mode == RUN :
+
+        # we want to reconnect to running VMs, specified by their IDs
+        for vm_id in vm_ids :
+
+            print "\nreconnecting to id %s" % vm_id
+
+            # get a handle on that VM, and print some information
+            cr = rm.acquire (vm_id)
+
+            print "  id           : %s"       %  cr.id
+            print "  state        : %s (%s)"  %  (state2str(cr.state), cr.state_detail)
+            print "  access       : %s"       %  cr.access
+
+            # keep that instance in our list of resources to run jobs on
+            crs.append (cr)
+
+        # run a simple job on each compute resource (VM) in our list
+        for cr in crs :
+
+            print "\nConnecting to VM %s" % cr.id
+
+            # make sure the machine is not in final state already
+            state = cr.state
+            print "  state        : %s (%s)"  %  (state2str(cr.state), cr.state_detail)
+            if  state in [saga.resource.EXPIRED,
+                          saga.resource.DONE,
+                          saga.resource.FAILED] :
+                print "  VM %s is alrady in final state"  %  vm_id
+                continue
+
+            # we only can run jobs on ACTIVE machines -- so lets wait until the VM
+            # is in that state.
+            # Note: the careful coder will spot the subtle race condition between the 
+            # check above and the check on this line... ;-)
+            if state != saga.resource.ACTIVE :
+              cr.wait (saga.resource.ACTIVE)
+
+              # Once a VM comes active, it still needs to boot and setup the ssh
+              # daemon to be usable -- we thus wait for a while
+              # Note: this is a workaround and needs to be fixed in the adaptor!
+              time.sleep (60)
+
+            print "  state        : %s (%s)"  %  (state2str(cr.state), cr.state_detail)
+
+
+            # The session created above contains the ssh context to access the VM
+            # instance -- that context was created from the ec2_keypair context
+            # which was earlier used for VM contextualization.  So we use that
+            # session to create a job service instance for that VM:
+            js = saga.job.Service (cr.access, session=s)
+
+            print "Running job"
+            # all ready: do the deed!
+            j = js.run_job ('sleep 10')
+            print "  job state    : %s"  %  j.state
+            j.wait ()
+            print "  job state    : %s"  %  j.state
+
+
+
+
     else :  # mode == STOP
 
         # we want to reconnect to running VMs, specified by their IDs
@@ -251,58 +320,10 @@ def main () :
             crs.append (cr)
 
 
-    # in both, the running and destroy mode, we run a simple job on each compute
-    # resource (VM) in our list
-    for cr in crs :
-
-        print "\nConnecting to VM %s" % cr.id
-
-        # make sure the machine is not in final state already
-        state = cr.state
-        print "  state        : %s (%s)"  %  (state2str(cr.state), cr.state_detail)
-        if  state in [saga.resource.EXPIRED,
-                      saga.resource.DONE,
-                      saga.resource.FAILED] :
-            print "  VM %s is alrady in final state"  %  vm_id
-            continue
-
-        # we only can run jobs on ACTIVE machines -- so lets wait until the VM
-        # is in that state.
-        # Note: the careful coder will spot the subtle race condition between the 
-        # check above and the check on this line... ;-)
-        if state != saga.resource.ACTIVE :
-          cr.wait (saga.resource.ACTIVE)
-
-          # Once a VM comes active, it still needs to boot and setup the ssh
-          # daemon to be usable -- we thus wait for a while
-          # Note: this is a workaround and needs to be fixed in the adaptor!
-          time.sleep (60)
-
-        print "  state        : %s (%s)"  %  (state2str(cr.state), cr.state_detail)
-
-
-        # The session created above contains the ssh context to access the VM
-        # instance -- that context was created from the ec2_keypair context
-        # which was earlier used for VM contextualization.  So we use that
-        # session to create a job service instance for that VM:
-        js = saga.job.Service (cr.access, session=s)
-
-        print "Running job"
-        # all ready: do the deed!
-        j = js.run_job ('sleep 10')
-        print "  job state    : %s"  %  j.state
-        j.wait ()
-        print "  job state    : %s"  %  j.state
-
-
-    # after we run the job(s), we are done -- for the START case.  In the STOP
-    # case, we now shut down the VM instances
-    if mode == STOP :
         for cr in crs :
             print "\nshutting down %s (%s)" % (cr.id, state2str(cr.state))
             cr.destroy ()
             print "  state        : %s (%s)"  %  (state2str(cr.state), cr.state_detail)
-
 
 
 # ------------------------------------------------------------------------------
