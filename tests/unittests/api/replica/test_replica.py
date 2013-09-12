60b4ea1e3f883a50f21a5b74b5e5cdafb789d6cd
Ashley Z <anz7@rutgers.edu>
2013-09-12 12:31:16 -0400

    Fixing up official test suites

--- tests/unittests/api/replica/test_replica.py
+++ tests/unittests/api/replica/test_replica.py
@@ -6,9 +6,12 @@ __license__   = "MIT"
 import time
 import saga
 import saga.utils.test_config as sutc
+import os
 
 from copy import deepcopy
 
+FILE_SIZE = 1 # in megs, approx                                                                                                         
+NUM_REPLICAS = 5 # num replicas to create
 
 # ------------------------------------------------------------------------------
 #
@@ -19,7 +22,11 @@ def test_replica_entry():
         tc = sutc.TestConfig()
         the_url = tc.js_url # from test config file
         the_session = tc.session # from test config file
-
+        irods_url = tc.replica_url
+        print "Testing iRODS entry with URL: %s" % irods_url
+        print " *** NOTE: only localhost is supported, which makes use "\
+              "of the current iRODS installion/environment variables."
+        irods_directory = saga.replica.LogicalDirectory(irods_url)
         assert True
 
     except saga.SagaException as ex:
@@ -34,10 +41,64 @@ def test_replica_directory():
         tc = sutc.TestConfig()
         the_url = tc.js_url # from test config file
         the_session = tc.session # from test config file
+        irods_url = tc.replica_url
+        irods_directory = saga.replica.LogicalDirectory(irods_url)
+        assert True
+
+    except saga.SagaException as ex:
+        assert False, "unexpected exception %s" % ex
+
+# ------------------------------------------------------------------------------
+#
+def test_replica_directory_listing():
+    """ Test logical file directory listing
+    """
+    try:
+        tc = sutc.TestConfig()
+        the_url = tc.js_url # from test config file
+        the_session = tc.session # from test config file
+        irods_url = tc.replica_url
+        irods_directory = saga.replica.LogicalDirectory(irods_url)
+        
+        for entry in irods_directory.list():
+            print entry
 
         assert True
 
     except saga.SagaException as ex:
         assert False, "unexpected exception %s" % ex
 
+
+# ------------------------------------------------------------------------------
+#
+def test_upload_and_download():
+    """ Test iRODS file upload and download 
+    """
+    try:
+        tc = sutc.TestConfig()
+        home_dir = os.path.expanduser("~"+"/")
+        print "Creating temporary file of size %dM : %s" % \
+            (FILE_SIZE, home_dir+TEMP_FILENAME)
+
+        # create a file for us to use with iRODS                                                                                        
+        with open(home_dir+TEMP_FILENAME, "wb") as f:
+            f.write ("x" * (FILE_SIZE * pow(2,20)) )
+
+        print "Creating iRODS directory object"
+        mydir = saga.replica.LogicalDirectory("irods://localhost/" + IRODS_DIRECTORY)
+
+        import subprocess
+        subprocess.call(["irm", IRODS_DIRECTORY+TEMP_FILENAME])
+
+        print "Uploading file to iRODS"
+        myfile = saga.replica.LogicalFile('irods://'+IRODS_DIRECTORY+TEMP_FILENAME)
+        myfile.upload(home_dir + TEMP_FILENAME, \
+                     "irods:///this/path/is/ignored/?resource="+IRODS_RESOURCE)
+
+        print "Deleting file locally : %s" % (home_dir + TEMP_FILENAME)                                                   
+        os.remove(home_dir + TEMP_FILENAME)
+        assert True
+
+    except saga.SagaException as ex:
+        assert False, "unexpected exception %s" % ex
 
