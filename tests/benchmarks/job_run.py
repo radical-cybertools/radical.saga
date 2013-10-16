67a3bd58590c4bc13988e9d16a6bd559208627d3
Andre Merzky <andre@merzky.net>
2013-10-17 00:13:53 +0200

    benchmark fixes

--- tests/benchmarks/job_run.py
+++ tests/benchmarks/job_run.py
@@ -1,5 +1,5 @@
 
-import saga.utils.benchmark as sb
+import radical.utils.benchmark as rb
 
 import os
 import sys
@@ -51,7 +51,7 @@ def benchmark_post (tid, args={}) :
 #
 try:
 
-    sb.benchmark_init ('job_run', benchmark_pre, benchmark_core, benchmark_post)
+    rb.benchmark_init ('job_run', benchmark_pre, benchmark_core, benchmark_post)
 
 except saga.SagaException, ex:
     print "An exception occured: (%s) %s " % (ex.type, (str(ex)))
