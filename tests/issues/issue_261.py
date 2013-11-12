da8b94260057351961c50c4359d124a117c1c917
Andre Merzky <andre@merzky.net>
2013-11-12 02:59:33 +0100

    fix recursive copy for sftp, un a cumbersome way.  This is for #261.

--- tests/issues/issue_261.py
+++ tests/issues/issue_261.py
@@ -2,19 +2,20 @@
 import saga
 import os
 
-remote_tmp = saga.filesystem.Directory ("ssh://india.futuregrid.org/tmp/copy_test", saga.filesystem.CREATE_PARENTS)
+remote_tmp = saga.filesystem.Directory ("sftp://india.futuregrid.org/tmp/copy_test", saga.filesystem.CREATE_PARENTS)
+
 print "initial:"
 for u in remote_tmp.list () :
     print str(u)
 
-local_dir  = saga.filesystem.Directory ("file://localhost/etc/")
-local_dir.copy ('host*', "ssh://india.futuregrid.org/tmp/copy_test")
+local_dir  = saga.filesystem.Directory ("file:///etc")
+local_dir.copy ('h*', "sftp://india.futuregrid.org/tmp/copy_test", saga.filesystem.RECURSIVE)
 print "intermediate:"
 for u in remote_tmp.list () :
     print str(u)
 
 local_tmp = saga.filesystem.Directory ("file://localhost/tmp/copy_test", saga.filesystem.CREATE_PARENTS)
-remote_tmp.copy ('host*', "file://localhost/tmp/copy_test")
+remote_tmp.copy ('h*', "file://localhost/tmp/copy_test", saga.filesystem.RECURSIVE)
 print "final:"
 for u in local_tmp.list () :
     print str(u)
