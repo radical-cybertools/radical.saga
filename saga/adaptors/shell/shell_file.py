1d235052295f94f40d8c38356c8c5b5b15ff8753
Andre Merzky <andre@merzky.net>
2013-11-12 01:00:06 +0100

    support list on empty dirs.  Duh!

--- saga/adaptors/shell/shell_file.py
+++ saga/adaptors/shell/shell_file.py
@@ -364,9 +364,12 @@ class ShellDirectory (saga.adaptors.cpi.filesystem.Directory) :
         # FIXME: eval flags
 
         if  None == npat :
-            npat = "*"
+            npat = ""
+        else :
+            npat = "-d %s" % npat
 
-        ret, out, _ = self.shell.run_sync ("/bin/ls -C1 -d %s\n" % npat)
+        ret, out, _ = self.shell.run_sync ("/bin/ls -C1 %s\n" % npat)
+
         if  ret != 0 :
             raise saga.NoSuccess ("failed to list(): (%s)(%s)" \
                                % (ret, out))
