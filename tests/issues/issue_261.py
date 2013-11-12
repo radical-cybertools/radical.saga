9fd1bc9e8e948c22757ccbc88aadfdf8df336622
Andre Merzky <andre@merzky.net>
2013-11-12 01:10:43 +0100

    update issue test for #261

--- tests/issues/issue_261.py
+++ tests/issues/issue_261.py
@@ -2,13 +2,20 @@
 import saga
 import os
 
-remote_dir = saga.filesystem.Directory ("ssh://india.futuregrid.org/tmp/copy_test", saga.filesystem.CREATE_PARENTS)
-print "initial: %s" % str(remote_dir.list ())
+remote_tmp = saga.filesystem.Directory ("ssh://india.futuregrid.org/tmp/copy_test", saga.filesystem.CREATE_PARENTS)
+print "initial:"
+for u in remote_tmp.list () :
+    print str(u)
 
 local_dir  = saga.filesystem.Directory ("file://localhost/etc/")
 local_dir.copy ('host*', "ssh://india.futuregrid.org/tmp/copy_test")
-print "final:"
-for u in remote_dir.list () :
+print "intermediate:"
+for u in remote_tmp.list () :
     print str(u)
 
+local_tmp = saga.filesystem.Directory ("file://localhost/tmp/copy_test", saga.filesystem.CREATE_PARENTS)
+remote_tmp.copy ('host*', "file://localhost/tmp/copy_test")
+print "final:"
+for u in local_tmp.list () :
+    print str(u)
 
