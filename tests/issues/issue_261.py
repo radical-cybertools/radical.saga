dad28ef8cb4340e0e74f1e8eec5a17cfef9149bb
Andre Merzky <andre@merzky.net>
2013-11-12 01:00:18 +0100

    adjust issue test for #261

--- tests/issues/issue_261.py
+++ tests/issues/issue_261.py
@@ -2,10 +2,13 @@
 import saga
 import os
 
-local_dir = saga.filesystem.Directory ("file://localhost/etc/")
-local_dir.copy ('host*', "ssh://localhost/tmp/stage_test/")
+remote_dir = saga.filesystem.Directory ("ssh://india.futuregrid.org/tmp/copy_test", saga.filesystem.CREATE_PARENTS)
+print "initial: %s" % str(remote_dir.list ())
 
-check = os.popen ("ls -la /tmp/stage_test/")
-for line in check.readlines() :
-    print line[:-1]
+local_dir  = saga.filesystem.Directory ("file://localhost/etc/")
+local_dir.copy ('host*', "ssh://india.futuregrid.org/tmp/copy_test")
+print "final:"
+for u in remote_dir.list () :
+    print str(u)
+
 
