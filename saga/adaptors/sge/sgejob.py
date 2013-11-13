502237952ab7cb2f8c1a4707c24f5bd6705f6087
Alexander Grill <alexander.grill@tuwien.ac.at>
2013-11-13 12:26:54 +0100

    Added magic comment string to allow utf-8 encoding in the source file

--- saga/adaptors/sge/sgejob.py
+++ saga/adaptors/sge/sgejob.py
@@ -1,4 +1,4 @@
-
+# coding=utf-8
 __author__    = "Andre Merzky, Christian Pérez-Llamas, Ole Weidner, Thomas Schatz, Alexander Grill"
 __copyright__ = "Copyright 2012-2013, The SAGA Project"
 __license__   = "MIT"
