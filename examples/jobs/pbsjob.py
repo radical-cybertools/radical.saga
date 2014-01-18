6b7b9d7d564d3cf9e921fa2b602665d5c7da5dd8
Ole Weidner <ole.weidner@rutgers.edu>
2014-01-18 17:49:26 -0500

    fixes #295

--- examples/jobs/pbsjob.py
+++ examples/jobs/pbsjob.py
@@ -41,7 +41,7 @@ def main():
         # Create a job service object that represent a remote pbs cluster.
         # The keyword 'pbs' in the url scheme triggers the PBS adaptors
         # and '+ssh' enables PBS remote access via SSH.
-        js = saga.job.Service("pbs+ssh://hotel.futuregrid.org",
+        js = saga.job.Service("pbs+ssh://india.futuregrid.org",
                               session=session)
 
         # Next, we describe the job we want to run. A complete set of job
--- saga/adaptors/sge/sgejob.py
+++ saga/adaptors/sge/sgejob.py
@@ -801,7 +801,7 @@ class SGEJobService (saga.adaptors.cpi.job.Service):
         # (1) we create a temporary file with 'mktemp' and write the contents of 
         #     the generated PBS script into it
         # (2) we call 'qsub <tmpfile>' to submit the script to the queueing system
-        cmdline = """SCRIPTFILE=`mktemp -t SAGA-Python-SGEJobScript.XXXXXX` &&  echo "%s" > $SCRIPTFILE && %s -notify $SCRIPTFILE""" %  (script, self._commands['qsub']['path'])
+        cmdline = """SCRIPTFILE=`mktemp -t SAGA-Python-SGEJobScript.XXXXXX` &&  echo "%s" > $SCRIPTFILE && %s -notify $SCRIPTFILE && rm -f $SCRIPTFILE""" %  (script, self._commands['qsub']['path'])
         #cmdline = 'echo "%s" | %s -notify' % (script, self._commands['qsub']['path'])
         ret, out, _ = self.shell.run_sync(cmdline)
 
