7a05754a3974d6029332cc18193e97456f5a80b9
Ole Weidner <oweidner@Oles-MacBook-Pro.local>
2014-02-24 22:18:55 -0500

    added minimum version of RU to avoid errors due to already installed, older RUs

--- setup.py
+++ setup.py
@@ -233,7 +233,7 @@ setup_args = {
     'cmdclass'             : {
         'test'             : our_test, 
     },
-    'install_requires'     : ['apache-libcloud', 'radical.utils'],
+    'install_requires'     : ['apache-libcloud', 'radical.utils>=0.6.0'],
     'tests_require'        : ['nose'],
     'zip_safe'             : False,
 }
