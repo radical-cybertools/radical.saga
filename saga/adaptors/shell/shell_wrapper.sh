20ea8905d9d43b760092bda54212f25691fab756
Andre Merzky <andre@merzky.net>
2014-02-26 19:07:58 +0100

    improve cleanup of shell adaptor tmp files
    This might improve performance on MacOS specifically.

--- saga/adaptors/shell/shell_wrapper.sh
+++ saga/adaptors/shell/shell_wrapper.sh
@@ -633,6 +633,21 @@ cmd_purge () {
 
 # --------------------------------------------------------------------
 #
+# purge tmp files for bulks etc.
+#
+cmd_purge_tmps () {
+
+  rm -f "$BASE"/bulk.*
+  rm -f "$BASE"/idle.*
+  rm -f "$BASE"/quit.*
+  find  "$BASE" -type d -mtime +30 -exec rm -rf {} \;
+  find  "$BASE" -type f -mtime +30 -exec rm -rf {} \;
+  RETVAL="purged tmp files"
+}
+
+
+# --------------------------------------------------------------------
+#
 # quit this script gracefully
 #
 cmd_quit () {
@@ -795,11 +810,12 @@ listen() {
 \stty -echo   2> /dev/null
 \stty -echonl 2> /dev/null
 
-# FIXME: this leads to timing issues -- disabled for benchmarking
-# if test "$PURGE_ON_START" = "True"
-# then
-#   cmd_purge
-# fi
+# FIXME: this leads to timing issues -- disable for benchmarking
+if test "$PURGE_ON_START" = "True"
+then
+  cmd_purge
+  cmd_purge_tmps
+fi
 
 create_monitor
 listen $1
