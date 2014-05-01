2802b372a09b1fca59185e241c947ed08e1bd3dc
Andre Merzky <andre@merzky.net>
2014-05-02 00:55:49 +0200

    use getpass for token read

--- saga/utils/pty_shell_factory.py
+++ saga/utils/pty_shell_factory.py
@@ -308,8 +308,8 @@ class PTYShellFactory (object) :
                     # --------------------------------------------------------------
                     elif n == 2 :
                         logger.info ("got token prompt")
-                        sys.stdout.write ("enter token: \n")
-                        token = sys.stdin.readline ()
+                        import getpass
+                        token = getpass.getpass ("enter token: ")
                         pty_shell.write ("%s\n" % token.strip())
                         n, match = pty_shell.find (prompt_patterns, delay)
 
