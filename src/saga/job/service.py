2c2534acec944677a9bc2d0f6d9e7d636e9d686d
mingtaiha <ming.tai.ha@gmail.com>
2016-07-22 21:12:37 +0200

    Made changes in accordance to GFD90

--- src/saga/job/service.py
+++ src/saga/job/service.py
@@ -321,13 +321,18 @@ class Service (sb.Base, sasync.Async) :
         .. warning:: |not_implemented|
         """
 
+    # Create Job and then Run Job via <Adaptor>Run
         if not self.valid :
             raise se.IncorrectState ("This instance was already closed.")
 
         if  None == host :
             host = "" # FIXME
-        return self._adaptor.run()
+
+        js = self._adaptor.create_job(cmd)
+        js._adaptor.run()
+        return js
         #return self._adaptor.run_job (cmd, host, ttype=ttype)
+
 
 
     # --------------------------------------------------------------------------
