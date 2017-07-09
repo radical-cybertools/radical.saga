8ab6a2e36ce814872903cdb2dfc6c19f253337bc
Andre Merzky <andre@merzky.net>
2017-07-09 15:57:46 +0200

    more thorough handling of $PROMPT_COMMAND

--- src/saga/utils/pty_shell.py
+++ src/saga/utils/pty_shell.py
@@ -303,10 +303,10 @@ class PTYShell (object) :
                 # a versatile prompt pattern to account for the custom shell case.
                 try :
                     # set and register new prompt
-                    self.run_async  ( " unset PROMPT_COMMAND;"
-                                    + " set HISTFILE=$HOME/.saga_history;"
+                    self.run_async  ( " set HISTFILE=$HOME/.saga_history;"
                                     + " PS1='PROMPT-$?->';"
                                     + " PS2='';"
+                                    + " PROMPT_COMMAND='';"
                                     + " export PS1 PS2 2>&1 >/dev/null;"
                                     + " cd $HOME 2>&1 >/dev/null\n")
                     self.set_prompt (new_prompt="PROMPT-(\d+)->$")
