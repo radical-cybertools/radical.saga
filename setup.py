8d8812e6166556989272525c2c47de87700e9f54
Andre Merzky <andre@merzky.net>
2018-03-08 15:10:07 -0500

    add coverage

--- setup.py
+++ setup.py
@@ -145,9 +145,10 @@ class our_test(Command):
     def finalize_options   (self) : pass
     def run (self) :
         testdir = "%s/tests/" % os.path.dirname(os.path.realpath(__file__))
-        retval  = sp.call([sys.executable,
-                          '%s/run_tests.py'               % testdir,
-                          '%s/configs/basetests.cfg'      % testdir])
+        retval  = sp.call(['coverage',
+                           'run',
+                           '%s/run_tests.py'          % testdir,
+                           '%s/configs/basetests.cfg' % testdir])
         raise SystemExit(retval)
 
 
@@ -277,7 +278,7 @@ setup_args = {
     'install_requires'   : ['apache-libcloud', 
                             'radical.utils',
                             'parse'],
-    'tests_require'      : [],
+    'tests_require'      : ['nose', 'coverage'],
     'test_suite'         : 'saga.tests',
     'zip_safe'           : False,
 #   'build_sphinx'       : {
