fe08abbfa12040054812f38d49908a88ab84e9a3
Ioannis Paraskevakos <i.paraskev@rutgers.edu>
2018-08-30 17:38:11 -0400

    Fixed test bug

--- tests/adaptors/lsf/test_lsf_adaptor.py
+++ tests/adaptors/lsf/test_lsf_adaptor.py
@@ -35,23 +35,23 @@ class TestGenerator(unittest.TestCase):
         self._jd.total_node_count = 2
         self._jd.alloc_flags = ['gpumps', 'smt4']
         
-        self._script = "\n#!/bin/bash \n\
-                          #BSUB -J Test \n\
-                          #BSUB -o output.log \n\
-                          #BSUB -e error.log \n\
-                          #BSUB -W 1:10 \n\
-                          #BSUB -q normal-queue \n\
-                          #BSUB -P TestProject \n\
-                          #BSUB -n 24 \n\
-                          #BSUB -nnodes 2 \n\
-                          #BSUB -alloc_flags gpumps, smt4 \n\n\
-                          export  test_env=15 \n\
-                          /bin/sleep 60 "
+        self._script = "\n#!/bin/bash \n" +\
+                         "#BSUB -J Test \n" +\
+                         "#BSUB -o output.log \n" +\
+                         "#BSUB -e error.log \n" +\
+                         "#BSUB -W 1:10 \n" +\
+                         "#BSUB -q normal-queue \n" +\
+                         "#BSUB -P TestProject \n" +\
+                         "#BSUB -n 24 \n" +\
+                         "#BSUB -nnodes 2 \n" +\
+                         "#BSUB -alloc_flags gpumps, smt4 \n\n" +\
+                         "export  test_env=15 \n" +\
+                         "/bin/sleep 60 "
 
     def test_lsfscript_generator(self):
         script = _lsfscript_generator(url=None, logger=None, jd=self._jd, ppn=None,
                                       lsf_version=None, queue=None, span=None)
-
+        print self._script
         self.assertTrue(script == self._script)
 
 if __name__ == "__main__":
