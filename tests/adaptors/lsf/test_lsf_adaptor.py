eb714fdc196eb9d09c9683b57955b359a85d2e61
Andre Merzky <andre@merzky.net>
2019-07-09 11:56:27 +0200

    simplify lsf test and adaptor code

--- tests/adaptors/lsf/test_lsf_adaptor.py
+++ tests/adaptors/lsf/test_lsf_adaptor.py
@@ -1,66 +1,65 @@
 #!/usr/bin/env python
 
-__author__ = "Ioannis Paraskevakos"
-__copyright__ = "Copyright 2018-2019, The SAGA Project"
-__license__ = "MIT"
+__author__    = 'Ioannis Paraskevakos'
+__copyright__ = 'Copyright 2018-2019, The SAGA Project'
+__license__   = 'MIT'
 
 
-""" This test tests the LSF script generator function as well as the LSF adaptor
-"""
+'''
+This test tests the LSF script generator function as well as the LSF adaptor
+'''
+
 import unittest
 
 import radical.saga.url as surl
 import radical.saga     as rs
 
-
-# ------------------------------------------------------------------------------
-#
-class TestGenerator(unittest.TestCase):
-
-    def setUp(self):
-      # self._url = surl.Url('gsissh://summit.ccs.ornl.gov')
-        self._url = surl.Url('gsissh://localhost')
-        self._jd                 = rs.job.Description()
-        self._jd.name            = 'Test'
-        self._jd.executable      = '/bin/sleep'
-        self._jd.arguments       = 60
-        self._jd.environment     = {'test_env': 15}
-        self._jd.output          = 'output.log'
-        self._jd.error           = 'error.log'
-        self._jd.queue           = 'normal-queue'
-        self._jd.project         = 'TestProject'
-        self._jd.wall_time_limit = 70
-        self._jd.total_cpu_count = 65
-
-        self._script = "\n#!/bin/bash \n" \
-                     + "#BSUB -J Test \n" \
-                     + "#BSUB -o output.log \n" \
-                     + "#BSUB -e error.log \n" \
-                     + "#BSUB -W 1:10 \n" \
-                     + "#BSUB -q normal-queue \n" \
-                     + "#BSUB -P TestProject \n" \
-                     + "#BSUB -nnodes 2 \n" \
-                     + "#BSUB -alloc_flags 'gpumps smt4' \n\n" \
-                     + "export  test_env=15 \n" \
-                     + "/bin/sleep 60 "
-
-    def test_lsfscript_generator(self):
-
-        from radical.saga.adaptors.lsf.lsfjob import _lsfscript_generator
-
-        script = _lsfscript_generator(url=self._url, logger=None, jd=self._jd,
-                                      ppn=None, lsf_version=None, queue=None,
-                                      span=None)
-
-        self.assertTrue(script == self._script)
+from radical.saga.adaptors.lsf.lsfjob import _lsfscript_generator
 
 
 # ------------------------------------------------------------------------------
 #
-if __name__ == "__main__":
+def test_lsfscript_generator():
 
-    suite = unittest.TestLoader().loadTestsFromTestCase(TestGenerator)
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    smt = 4
+    url = surl.Url('gsissh://summit.ccs.ornl.gov')
+    jd  = rs.job.Description()
+
+    jd.name            = 'Test'
+    jd.executable      = '/bin/sleep'
+    jd.arguments       = 60
+    jd.environment     = {'test_env': 15}
+    jd.output          = 'output.log'
+    jd.error           = 'error.log'
+    jd.queue           = 'normal-queue'
+    jd.project         = 'TestProject'
+    jd.wall_time_limit = 70
+    jd.total_cpu_count = 65 * smt
+
+    tgt_script = '\n#!/bin/bash \n' \
+               + '#BSUB -q normal-queue \n' \
+               + '#BSUB -J Test \n' \
+               + '#BSUB -W 1:10 \n' \
+               + '#BSUB -o output.log \n' \
+               + '#BSUB -e error.log \n' \
+               + '#BSUB -P TestProject \n' \
+               + '#BSUB -nnodes 2 \n' \
+               + "#BSUB -alloc_flags 'gpumps smt4' \n" \
+               + '\n' \
+               + 'export RADICAL_SAGA_SMT=%d test_env=15\n' % smt \
+               + '/bin/sleep 60'
+
+    script = _lsfscript_generator(url=url, logger=None, jd=jd,
+                                  ppn=None, lsf_version=None, queue=None)
+
+    assert (script == tgt_script)
+
+
+# ------------------------------------------------------------------------------
+#
+if __name__ == '__main__':
+
+    test_lsfscript_generator()
 
 
 # ------------------------------------------------------------------------------
