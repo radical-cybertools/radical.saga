96c13adb32d1e3e2f22b0df32a4ffc107f1bda81
Andre Merzky <andre@merzky.net>
2022-12-05 16:42:33 +0100

    typo

--- setup.py
+++ setup.py
@@ -212,7 +212,7 @@ setup_args = {
     'project_urls'       : {
         'Documentation': 'https://radical%s.readthedocs.io/en/latest/' % base,
         'Source'       : 'https://github.com/radical-cybertools/%s/'   % name,
-        'Issues' : 'https://github.com/radical-cybertools/%s/issues'   % name'
+        'Issues' : 'https://github.com/radical-cybertools/%s/issues'   % name,
     },
     'license'            : 'MIT',
     'keywords'           : 'radical job saga',
