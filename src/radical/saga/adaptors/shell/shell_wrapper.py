670e3f4814fc88681f0006cffe0fce445ca097b9
M. Eric Irrgang <ericirrgang@gmail.com>
2023-01-06 16:19:34 +0300

    Let shell_wrapper.py close file more promptly.

--- src/radical/saga/adaptors/shell/shell_wrapper.py
+++ src/radical/saga/adaptors/shell/shell_wrapper.py
@@ -8,5 +8,5 @@ import os
 
 # --------------------------------------------------------------------
 # server side job management script
-_WRAPPER_SCRIPT = open (os.path.dirname(__file__) + '/shell_wrapper.sh').read ()
-
+with open(os.path.dirname(__file__) + '/shell_wrapper.sh') as fh:
+    _WRAPPER_SCRIPT = fh.read ()
